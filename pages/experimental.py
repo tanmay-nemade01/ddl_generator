bab03f3f5c93c67d09fe5205d372b0f88bedb250
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2023-12-05 08:18:09 +0530

    Update experimental.py

--- pages/experimental.py
+++ pages/experimental.py
@@ -14,8 +14,8 @@ for i in range(len(primary_key_list)):
 primary_keys_stage = primary_keys_stage[:-1]
 
 table_count = 'select count(*) from ' + table_name +';'
-stage_count = 'select count(distinct'+primary_keys_stage+') from @STG_S3_CDP_CMACGM_LARA_PRD_R/awscdc_cds/LRA_SCE_M/'+table_name+'/  (FILE_FORMAT => /'FF_PARQUET/'));'
-op_d_count = 'select count(distinct'+primary_keys_stage+') from @STG_S3_CDP_CMACGM_LARA_PRD_R/awscdc_cds/LRA_SCE_M/'+table_name+'/  (FILE_FORMAT => /'FF_PARQUET/')) where $1:OP = /'D/';'
+stage_count = 'select count(distinct'+primary_keys_stage+') from @STG_S3_CDP_CMACGM_LARA_PRD_R/awscdc_cds/LRA_SCE_M/'+table_name+"/  (FILE_FORMAT => 'FF_PARQUET'));"
+op_d_count = 'select count(distinct'+primary_keys_stage+') from @STG_S3_CDP_CMACGM_LARA_PRD_R/awscdc_cds/LRA_SCE_M/'+table_name+"/  (FILE_FORMAT => 'FF_PARQUET')) where $1:OP = 'D';"
 table_count
 stage_count
 op_d_count
