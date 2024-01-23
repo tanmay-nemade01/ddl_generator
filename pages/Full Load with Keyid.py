5222ddfa761797017fda9886113507863c8ef5d8
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2024-01-24 00:28:55 +0530

    Update Full Load with Keyid.py

--- pages/Full Load with Keyid.py
+++ pages/Full Load with Keyid.py
@@ -618,3 +618,9 @@ if file is not None:
     script_template = script_template.replace('<table_key>',primary_keys)
 
     st.code(script_template)
+    st.download_button(
+	label="Download SQL Code",
+        data=script_template,
+	file_name=f"{environment}.{table_name}.sql",
+	mime="text/plain"
+	)
