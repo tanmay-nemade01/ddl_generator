048d9e4c9e72fa3a2adb6ceb51b81d64813b5df8
tanmay-nemade01 <152612566+tanmay-nemade01@users.noreply.github.com>
2024-03-13 13:00:09 +0530

    Update Full Load with Keyid.py

--- pages/Full Load with Keyid.py
+++ pages/Full Load with Keyid.py
@@ -24,6 +24,7 @@ def main_function(json_data, table_name, aws_url, environment, schema, primary_k
 
     name_list = []
     for i in range(length):
+        data['COLUMN_NAME'][i] = data['COLUMN_NAME'][i].replace(' ','')
         name_list.append(data['COLUMN_NAME'][i].upper())
 
     count = 0
@@ -31,6 +32,7 @@ def main_function(json_data, table_name, aws_url, environment, schema, primary_k
 
     type_list = []
     for i in range(length):
+        data['TYPE'][i] = data['TYPE'][i].replace(' ','')
         type_list.append(data['TYPE'][i].upper())
 
     if 'OP' in name_list:
